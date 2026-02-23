65eda65a43a54dd99683217f27ad10e568797375
anupamshukla18 <satyamshukla123kte@gmail.com>
2026-02-23 12:00:13 +0530

    [Anupam] : completed UC4

--- src/main/java/com/apps/quantitymeasurement/Length.java
+++ src/main/java/com/apps/quantitymeasurement/Length.java
@@ -7,7 +7,7 @@ public class Length {
 
 	// Enum inside class
 	public enum LengthUnit {
-		FEET(12.0), INCHES(1.0);
+		FEET(12.0), INCHES(1.0), YARDS(36.0), CENTIMETERS(0.393701);
 
 		private final double conversionFactor;
 
